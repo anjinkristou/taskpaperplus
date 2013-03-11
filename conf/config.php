33a92698ca9e13ddc8cd791b95bc87edf04fe8f1
Mehrez Kristou <mehrez@kristou.com>
2013-03-12 08:26:49 +0900

    Default config

--- conf/config.php
+++ conf/config.php
@@ -5,8 +5,8 @@
  */
 $config = array();
 
-$config['website_url']      = "http://tasks.kristou.com";
-$config['version_number']   = '1.0.0.0 alpha | 05-Nov-2012';
+$config['website_url']      = "http://github.com/MrBertie/taskpaper";
+$config['version_number']   = '1.0.0.0 alpha | 2013-Mar-11';
 $config['debug_file']       = 'logs/debug.txt';       // relative to App Base Path
 $config['log_file']         = 'logs/log.txt';
 $config['user_file']        = '_cache/users';
@@ -16,7 +16,6 @@ $config['cache_dir']        = '_cache/';
 $config['hide_tips']        = false;    // hide all pop up tool tips
 $config['edit_new_tab']     = true;     // open new tabs in 'edit' state
 
-$config['title']            = 'Kristou Tasks';
+$config['title']            = 'Taskpaper+';
 $config['default_active']   = 'tasks';
 $config['date_format']      = "%d-%b-%Y";   // strftime formatting!  See php help files
-?>
